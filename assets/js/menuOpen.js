be225db339830a54d5c957a001acf7c628bdd904
Matheus Rocha <01devmath@gmail.com>
2023-07-30 18:10:46 -0300

    feat: implement menu toggle function

--- assets/js/menuOpen.js
+++ assets/js/menuOpen.js
@@ -1,7 +1,24 @@
-// Toggle the menu open and close when on mobile
 export default function menuOpen() {
-    const burgerButton = document.querySelector('.gh-burger');
-    burgerButton.addEventListener('click', function () {
-        document.body.classList.toggle('gh-head-open');
-    });
+  const primaryNav = document.querySelector("#mobile-menu-2");
+  const navToggle = document.querySelector("#mobile-nav-toggle");
+  const closeIcon = document.querySelector("#menu-close-icon");
+  const openIcon = document.querySelector("#menu-open-icon");
+
+  navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility === "false") {
+      primaryNav.setAttribute("data-visible", true);
+      navToggle.setAttribute("aria-expanded", true);
+      primaryNav.classList.remove("hidden");
+      closeIcon.classList.remove("hidden");
+      openIcon.classList.add("hidden");
+    } else if (visibility === "true") {
+      primaryNav.setAttribute("data-visible", false);
+      navToggle.setAttribute("aria-expanded", false);
+      primaryNav.classList.add("hidden");
+      closeIcon.classList.add("hidden");
+      openIcon.classList.remove("hidden");
+    }
+  });
 }
